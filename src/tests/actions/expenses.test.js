d6f58202ad5a4545977a1cbca246e07c4a5cd50d
Alexander <adgbq2@mail.missouri.edu>
2019-09-06 13:27:56 -0600

    fixed jest battling with babel

---
+++ src/tests/actions/expenses.test.js
@@ -0,0 +1,53 @@
+import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+
+test('should setup remove expense action object', () => {
+    const action = removeExpense({ id: '123abc' })
+    expect(action).toEqual({
+        type: 'REMOVE_EXPENSE',
+        id: '123abc'
+    })
+})
+
+test('Should setup edit expense action object', () => {
+    const action = editExpense('123abc', { note: 'new note value'})
+    expect(action).toEqual({
+        type: 'EDIT_EXPENSE',
+        id: '123abc',
+        updates: {
+            note: 'new note value'
+        }
+    })
+})
+
+test('should setup add expense action object with provided values', () => {
+    const expenseData = {
+        description: 'Rent',
+        amount: 109500,
+        createdAt: 1000,
+        note: 'This is test'
+    }
+    const action = addExpense(expenseData)
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            ...expenseData,
+            id: expect.any(String)
+        }
+    })
+})
+
+
+test('Should setup add expense action object with default values', () => {
+    const action = addExpense()
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            id: expect.any(String),
+            description: '', 
+            note: '',
+            amount: 0,
+            createdAt: 0
+        }
+    })
+
+})
