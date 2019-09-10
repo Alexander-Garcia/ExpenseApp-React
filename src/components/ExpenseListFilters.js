47383701d587999f9a419d45751aa43f72236d0f
Alexander <adgbq2@mail.missouri.edu>
2019-09-10 15:26:57 -0600

    updated gitignore to ignore style source maps. Partially configured webpack for production builds

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -58,7 +58,7 @@ const mapStateToProps = (state) => ({
         filters: state.filters
 })
 
-const mapDispatchToProps = () => ({
+const mapDispatchToProps = (dispatch) => ({
     setTextFilter: (text) => dispatch(setTextFilter(text)),
     sortByDate: () => dispatch(sortByDate()),
     sortByAmount: () => dispatch(sortByAmount()),
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,59 @@
-const path = require('path'); 
+const path = require('path');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
-    entry: "./src/app.js",
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.resolve(__dirname, 'public'),
-        historyApiFallback: true
+module.exports = (env) => {
+    const isProduction = env === 'production'
+    const CSSExtract = new MiniCSSExtractPlugin({ filename: 'styles.css'})
+
+    return {
+        entry: "./src/app.js",
+        output: {
+            path: path.resolve(__dirname, 'public'),
+            filename: "bundle.js"
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: [
+                    {
+                        loader: MiniCSSExtractPlugin.loader
+                    }, {
+                        loader: 'css-loader',
+                        options: {
+                                sourceMap: true
+                            }
+                    }, {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.resolve(__dirname, 'public'),
+            historyApiFallback: true
+        }
     }
-};
+}
 
 // loader for babel helps webpack use babel to transpile JSX into javascript
-/*  
+/*
     This is in the yarn add babel-core  (babel-core is used by webpack)
     babel-loader is a webpack plugin that teaches webpack to run babel when encountering specific files
-    These are specified in the module object 
+    These are specified in the module object
     .js$ makes sure the file loaded ends in .js
     exclude: we want to exclude the node_modules. We do not want babel to run through those
-    Summary: every time we see a .js file it needs to run through babel. 
+    Summary: every time we see a .js file it needs to run through babel.
     To tell webpack we need to set this up create a separate config file for babel. Call is .babelrc
 */
 
