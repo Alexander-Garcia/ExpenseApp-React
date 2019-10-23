95ce1c97d0b45f258317faa66c71bec9ed6c9dc3
Alexander <adgbq2@mail.missouri.edu>
2019-10-23 10:25:32 -0600

    added new tests

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -2,6 +2,7 @@ import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 import { startAddExpense, addExpense, editExpense, removeExpense } from '../../actions/expenses';
 import expenses from '../fixtures/expenses'
+import database from '../../firebase/firebase'
 
 const createMockStore = configureMockStore([thunk])
 
@@ -42,13 +43,49 @@ test('should add expense to database and store', (done) => {
     }
 
     store.dispatch(startAddExpense(expenseData)).then(() => {
-        expect(1).toBe(1)
-        done()
-    })
+        const actions = store.getActions()
+        expect(actions[0]).toEqual({
+            type: 'ADD_EXPENSE',
+            expense: {
+                id: expect.any(String),
+                ...expenseData
+            }
+        })
 
+        return database.ref(`expenses/${actions[0].expense.id}`).once('value')
+            .then((snapshot) => {
+            expect(snapshot.val()).toEqual(expenseData)
+            done()
+        })
+    })
 })
 
-test('should add expense with defaults to database and store', () => {
+test('should add expense with defaults to database and store', (done) => {
+    const store = createMockStore({})
+    const expenseDefaults = {
+        description: '',
+        amount: 0,
+        note: '',
+        createdAt: 0
+    }
+
+
+    store.dispatch(startAddExpense()).then(() => {
+        const actions = store.getActions()
+        expect(actions[0]).toEqual({
+            type: 'ADD_EXPENSE',
+            expense: {
+                id: expect.any(String),
+                ...expenseDefaults
+            }
+        })
+
+        return database.ref(`expenses/${actions[0].expense.id}`).once('value')
+            .then((snapshot) => {
+            expect(snapshot.val()).toEqual(expenseDefaults)
+            done()
+        })
+    })
 
 })
 
