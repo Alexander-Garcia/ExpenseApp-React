2328b7616797cc37e3db726d81637b3e0c88634b
Garcia <alexander.garcia@dish.com>
2019-10-25 14:44:43 -0600

    updated private routes for when logged in

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,6 +1,5 @@
 import React from 'react'; 
 import { Router, Route, Switch} from 'react-router-dom';
-import Header from '../components/Header'; 
 import AddExpensePage from '../components/AddExpensePage'; 
 import { createBrowserHistory } from 'history'
 import EditExpensePage from '../components/EditExpensePage'; 
@@ -8,6 +7,7 @@ import {ExpenseDashboardPage} from '../components/ExpenseDashboardPage';
 import {HelpPage} from '../components/HelpPage'; 
 import {NotFoundPage} from '../components/NotFoundPage'; 
 import LoginPage from '../components/LoginPage'
+import PrivateRoute from './PrivateRoute'
 /*
 
 Path of "/" is the root 'landing' page
@@ -28,12 +28,11 @@ export const history = createBrowserHistory()
 const AppRouter = () => (
     <Router history={history}>
         <div>
-            <Header />
             <Switch>
                 <Route path="/" component={LoginPage} exact={true} />
-                <Route path="/dashboard" component={ExpenseDashboardPage} />
-                <Route path="/create" component={AddExpensePage} />
-                <Route path="/edit/:id" component={EditExpensePage} />
+                <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
+                <PrivateRoute path="/create" component={AddExpensePage} />
+                <PrivateRoute path="/edit/:id" component={EditExpensePage} />
                 <Route path="/help" component={HelpPage} />
                 <Route component={NotFoundPage} />
             </Switch>
