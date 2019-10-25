1930b14226afc950ac8f9c338135ae03ecc06e61
Garcia <alexander.garcia@dish.com>
2019-10-25 08:44:08 -0600

    add async remove expense call

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -44,6 +44,14 @@ export const startAddExpense = (expenseData = {}) => {
     id
 })
 
+export const startRemoveExpense = ({ id } = {}) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).remove().then(() => {
+            dispatch(removeExpense({ id }))
+        })
+    }
+}
+
 
 //     EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
