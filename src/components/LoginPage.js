729c16bb26a09e489a50a5e771afa2f66234ea40
Garcia <alexander.garcia@dish.com>
2019-10-25 10:44:28 -0600

    added login component

---
+++ src/components/LoginPage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+export const LoginPage = () => (
+    <div>
+        <button>Login</button>
+    </div>
+)
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -6,6 +6,7 @@ import EditExpensePage from '../components/EditExpensePage';
 import {ExpenseDashboardPage} from '../components/ExpenseDashboardPage'; 
 import {HelpPage} from '../components/HelpPage'; 
 import {NotFoundPage} from '../components/NotFoundPage'; 
+import { LoginPage } from '../components/LoginPage'
 /*
 
 Path of "/" is the root 'landing' page
@@ -25,7 +26,8 @@ const AppRouter = () => (
         <div>
             <Header />
             <Switch>
-                <Route path="/" component={ExpenseDashboardPage} exact={true} />
+                <Route path="/" component={LoginPage} exact={true} />
+                <Route path="/dashboard" component={ExpenseDashboardPage} />
                 <Route path="/create" component={AddExpensePage} />
                 <Route path="/edit/:id" component={EditExpensePage} />
                 <Route path="/help" component={HelpPage} />
