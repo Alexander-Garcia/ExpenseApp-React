1fee663e960312977b5b68e74e7887c15560dca6
Garcia <alexander.garcia@dish.com>
2019-11-01 14:09:35 -0600

    styled header and summary

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -3,13 +3,19 @@ import selectExpenses from '../selectors/expenses'
 import selectExpensesTotal from '../selectors/expenses-total'
 import { connect } from 'react-redux'
 import numeral from 'numeral'
+import { Link } from 'react-router-dom'
 
 export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
     const expenseWord = expenseCount === 1 ? 'expense' : 'expenses'
     const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00')
     return (
-        <div>
-            <h1>Viewing {expenseCount} {expenseWord} totalling {formattedExpensesTotal} </h1>
+        <div className="page-header">
+            <div className="content-container">
+                <h1 className="page-header__title">Viewing <span>{expenseCount}</span> {expenseWord} totalling <span>{formattedExpensesTotal}</span> </h1>
+                <div className="page-header__actions">
+                    <Link className="button" to="/create">Add Expense</Link>
+                </div>
+            </div>
         </div>
     )
 }
